9a94e80f2a28d03e826dd0abf99f9c479e07d311
Mahima Ramgolam <mahima@ramgolam.com>
2020-05-29 21:42:54 +0400

    work in progress

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div
-    class="flex min-h-screen flex-col content-center py-12 justify-center text-2xl md:text-5xl"
+    class="flex min-h-screen flex-col content-center justify-center text-2xl md:text-5xl"
   >
-      <div class="text-orange-700 font-bold text-center px-4 py-2 m-2"> Sathya Sai International Organisation</div>
+      <div class="text-orange-600 font-bold text-center px-4 py-2 m-2"> Sathya Sai International Organisation</div>
       <div class="text-gray-700 text-center my-12 ">
         <img class="mx-auto h-full w-auto" src="http://saibabamauritius.org/sites/all/themes/surya/logo.png"/>
         </div>
-      <div class="text-orange-700 font-bold text-center px-4 py-2 m-2">Coming soon ...</div>
+      <div class="text-orange-600 font-bold text-center px-4 py-2 m-2">Coming soon ...</div>
 
   </div>
 </template>
