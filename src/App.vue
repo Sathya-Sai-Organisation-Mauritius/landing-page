856c0dd79135c99b930c582519172b87a8573f94
Sandeep Ramgolam <sandeep@ramgolam.com>
2020-05-29 20:11:17 +0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="flex min-h-screen flex-col items-center justify-center text-2xl md:text-5xl"
+  >
+    Sathya Sai International Organisation - Mauritius
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+};
+</script>
+
+<style lang="scss">
+* {
+  padding: 0;
+  border: 0;
+  margin: 0;
+}
+</style>
