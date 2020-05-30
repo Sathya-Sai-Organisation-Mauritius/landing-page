69874246a94bb1899cf15ab85f3200d37beac0f4
Mahima Ramgolam <mahima@ramgolam.com>
2020-05-30 21:32:34 +0400

    WIP edited

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
   <div
     class="flex bg-gray-200 font-sans min-h-screen flex-col content-center justify-center text-2xl md:text-5xl"
   >
-      <div class="fade-in text-orange-600 font-medium text-2xl md:text-5xl text-center px-4  py-2 mx-2 my-2"> Sathya Sai</div>
-      <div class="fade-in text-orange-600 font-medium text-2xl md:text-5xl text-center px-4  py-2 mx-2 mb-2">International Organisation</div>
-      <div class="fade-in text-orange-600 font-bold text-2xl md:text-6xl text-center px-4 py-2 mx-2 mb-2">Mauritius </div>
+      <div class="fade-in text-orange-600 font-medium text-2xl md:text-5xl text-center px-4  md:py-2 mx-2 md:mt-2"> Sathya Sai</div>
+      <div class="fade-in text-orange-600 font-medium text-2xl md:text-5xl text-center px-4  md:pb-2 mx-2 md:mb-2">International Organisation</div>
+      <div class="fade-in text-orange-600 font-bold text-4xl md:text-6xl text-center px-4 py-2 mx-2 mb-2">Mauritius </div>
       <div class="fade-in text-center my-12">
         <img class="mx-auto h-full w-48 md:w-auto" src="http://saibabamauritius.org/sites/all/themes/surya/logo.png"/>
         </div>
