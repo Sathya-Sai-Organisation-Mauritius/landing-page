841d108462893a8892f90e98f8ef5ee6a45a54ce
Mahima Ramgolam <mahima@ramgolam.com>
2020-05-30 21:26:22 +0400

    Fade in + WIP edited

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@
   <div
     class="flex bg-gray-200 font-sans min-h-screen flex-col content-center justify-center text-2xl md:text-5xl"
   >
-      <div class="text-orange-600 font-bold text-2xl md:text-5xl text-center px-4  md:pt-2 mx-2 mt-2"> Sathya Sai International Organisation</div>
-      <div class="text-orange-600 font-medium text-2xl md:text-5xl text-center px-4 pt-2 md:pt-0 pb-2 mx-2 mb-2">Mauritius </div>
-      <div class="text-center my-12">
+      <div class="fade-in text-orange-600 font-medium text-2xl md:text-5xl text-center px-4  py-2 mx-2 my-2"> Sathya Sai</div>
+      <div class="fade-in text-orange-600 font-medium text-2xl md:text-5xl text-center px-4  py-2 mx-2 mb-2">International Organisation</div>
+      <div class="fade-in text-orange-600 font-bold text-2xl md:text-6xl text-center px-4 py-2 mx-2 mb-2">Mauritius </div>
+      <div class="fade-in text-center my-12">
         <img class="mx-auto h-full w-48 md:w-auto" src="http://saibabamauritius.org/sites/all/themes/surya/logo.png"/>
         </div>
-      <div class="text-orange-600 font-bold text-2xl md:text-4xl text-center px-4 py-2 m-2">Coming soon... </div>
+        
+      <div class="fade-in text-orange-600 font-medium text-2xl md:text-4xl text-center px-4 py-2 m-2">Coming soon...</div>
   </div>
 </template>
 
@@ -23,5 +25,57 @@ export default {
   padding: 0;
   border: 0;
   margin: 0;
+}
+
+.fade-in {
+  animation: fadeIn ease 5s;
+  -webkit-animation: fadeIn ease 5s;
+  -moz-animation: fadeIn ease 5s;
+  -o-animation: fadeIn ease 5s;
+  -ms-animation: fadeIn ease 5s;
+}
+@keyframes fadeIn {
+  0% {
+    opacity:0;
+  }
+  100% {
+    opacity:1;
+  }
+}
+
+@-moz-keyframes fadeIn {
+  0% {
+    opacity:0;
+  }
+  100% {
+    opacity:1;
+  }
+}
+
+@-webkit-keyframes fadeIn {
+  0% {
+    opacity:0;
+  }
+  100% {
+    opacity:1;
+  }
+}
+
+@-o-keyframes fadeIn {
+  0% {
+    opacity:0;
+  }
+  100% {
+    opacity:1;
+  }
+}
+
+@-ms-keyframes fadeIn {
+  0% {
+    opacity:0;
+  }
+  100% {
+    opacity:1;
+}
 }
 </style>
