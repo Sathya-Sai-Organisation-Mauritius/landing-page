247b74fef9800fd6f31844b6a2030f6ec2cea94f
Mahima Ramgolam <mahima@ramgolam.com>
2020-05-31 23:51:30 +0400

    work in prgress changes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,98 @@
 <template>
   
-  <div
-    class="flex bg-gray-200 font-sans min-h-screen flex-col content-center justify-center text-2xl md:text-5xl"
-  >
-      <div class="fade-in-1 text-orange-600 font-medium text-2xl md:text-5xl text-center px-4 pt-2 mx-2 mt-2">Sathya Sai</div>
-      <div class="fade-in-2 text-orange-600 font-medium text-2xl md:text-5xl text-center px-4 pb-2 mx-2 mb-2">International Organisation</div>
+  <!--simple version-->
+  <!-- <div class="flex bg-gray-200 font-sans min-h-screen flex-col content-center justify-center text-2xl md:text-5xl">
+      <div class="transition duration-500 ease-in-out text-orange-600 font-medium text-2xl md:text-5xl text-center px-4 pt-2 mx-2 mt-2">Sathya Sai</div>
+      <div class="transition delay-150 ease-in-out text-orange-600 font-medium text-2xl md:text-5xl text-center px-4 pb-2 mx-2 mb-2">International Organisation</div>
       <div class="fade-in-3 text-orange-600 font-bold text-4xl md:text-6xl text-center px-4 py-2 mx-2 my-2">Mauritius </div>
       <div class="fade-in-4 text-center py-2 my-4">
         <img class="mx-auto h-full w-48 md:w-auto" src="http://saibabamauritius.org/sites/all/themes/surya/logo.png"/>
-        </div>
-        
+      </div>
+       
+      
       <div class="fade-in-5 text-orange-600 font-medium text-2xl md:text-4xl text-center px-4 py-2 m-2">Coming soon...</div>
+  </div> -->
+
+<!--other version-->
+  <!-- <div class=" border-4 border-green-500 bg-yellow-200">
+          <div
+            class="min-h-screen flex items-center justify-center py-4 px-4 sm:px-6 lg:py-24 lg:px-4"
+          >
+              <div class="relative flex justify-center">
+               <div
+                class="flex bg-gray-200 font-sans min-h-screen flex-col content-center justify-center text-2xl md:text-5xl"
+              >
+                  <div class="fade-in-1 text-orange-600 font-medium text-2xl md:text-5xl text-center px-4 pt-2 mx-2 mt-2">Sathya Sai</div>
+                  <div class="fade-in-2 text-orange-600 font-medium text-2xl md:text-5xl text-center px-4 pb-2 mx-2 mb-2">International Organisation</div>
+                  <div class="fade-in-3 text-orange-600 font-bold text-4xl md:text-6xl text-center px-4 py-2 mx-2 my-2">Mauritius </div>
+                  <div class="fade-in-4 text-center py-2 my-4">
+                    <img class="mx-auto h-full w-48 md:w-auto" src="http://saibabamauritius.org/sites/all/themes/surya/logo.png"/>
+                  </div>
+                  
+                  
+                  <div class="fade-in-5 text-orange-600 font-medium text-2xl md:text-4xl text-center px-4 py-2 m-2">Coming soon...</div>
+              </div>
+                  
+              </div>
+              <div class="relative">
+                <div>
+                  <img
+                    class=" max-h-screen "
+                    src="https://www.freevector.com/uploads/vector/preview/17179/FreeVector-Blue-Flowers-Background.jpg"
+                  />
+                </div>
+              </div>
+            
+          </div>
+        </div> -->
+
+
+
+
+
+
+<!-- <div class="relative min-h-screen overflow-hidden bg-gray-200">
+  <div class="hidden lg:block absolute scroll-bg" style="height: 400%;width: 400%;top: -25%;left: -100%;background-size: 800px auto;background-image: url('https://www.freevector.com/uploads/vector/preview/17179/FreeVector-Blue-Flowers-Background.jpg');"></div>
+  <div class="relative min-h-screen lg:min-w-3xl xl:min-w-4xl lg:flex lg:items-center lg:justify-center lg:w-3/5 lg:py-20 lg:pl-8 lg:pr-8 bg-no-repeat" style="background-image: url('https://melodicdistraction.com/wp-content/uploads/2017/10/black-paper-background.jpg'); background-size: 100% auto; background-position: -5px -5px;">
+    <div class="flex bg-gray-200 font-sans min-h-screen flex-col content-center justify-center text-2xl md:text-5xl">
+      <div class="fade-in-1 duration-500 ease-in-out text-orange-600 font-medium text-2xl md:text-5xl text-center px-4 mx-2">Sathya Sai</div>
+      <div class="fade-in-2 delay-150 ease-in-out text-orange-600 font-medium text-2xl md:text-5xl text-center px-4 pb-2 mx-2 mb-2">International Organisation</div>
+      <div class="fade-in-3 text-orange-600 font-bold text-4xl md:text-6xl text-center px-4 py-2 mx-2 my-2">Mauritius </div>
+      <div class="fade-in-4 text-center py-2 my-4">
+        <img class="mx-auto h-full w-48 md:w-auto" src="http://saibabamauritius.org/sites/all/themes/surya/logo.png"/>
+      </div>
+      <div class="fade-in-5 text-orange-600 font-medium text-2xl md:text-4xl text-center px-4 py-2 m-2">Coming soon...</div>
+    </div>
   </div>
+  <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+    <div>
+        <img class="min-h-screen"
+             src="https://www.freevector.com/uploads/vector/preview/17179/FreeVector-Blue-Flowers-Background.jpg"/>
+    </div>
+  </div>
+</div> -->
+
+<div class="relative min-h-screen overflow-hidden bg-gray-900 lg:bg-gray-300">
+  <div class="hidden lg:block absolute scroll-bg" style="height: 100%; width: 100%; top: -25%; left: -100%; background-size: 800px auto; background-image: url('');"></div>
+    <div class="relative min-h-screen lg:min-w-3xl xl:min-w-4xl lg:flex lg:items-center lg:justify-center lg:w-3/5 lg:py-20 lg:pl-8 lg:pr-8 bg-no-repeat" style="background-image: url('https://tailwindui.com/img/angled-background.svg');background-size: 100% auto;background-position: -5px -5px;">
+      <div>
+        <div class="px-6 md:max-w-3xl md:mx-auto lg:mx-0 lg:max-w-none lg:pt-0 lg:pb-16">
+          <div class="flex items-center justify-between">
+            <div class="flex font-sans min-h-screen flex-col content-center justify-around text-2xl md:text-5xl">
+                <div class="fade-in-1 duration-500 ease-in-out text-orange-600 font-medium text-2xl md:text-5xl text-center px-4 mx-2 ">Sathya Sai</div>
+                <div class="fade-in-2 delay-150 ease-in-out text-orange-600 font-medium text-2xl md:text-5xl text-center px-4 pb-2 mx-2 mb-2 -mt-16">International Organisation</div>
+                <div class="fade-in-3 text-orange-600 font-bold text-4xl md:text-6xl text-center px-4 py-2 mx-2 my-2">Mauritius </div>
+                <div class="fade-in-4 text-center py-2 my-4">
+                  <img class="mx-auto h-full w-48 md:w-auto" src="http://saibabamauritius.org/sites/all/themes/surya/logo.png"/>
+                </div>
+                <div class="fade-in-5 text-orange-600 font-medium tesxt-2xl md:text-4xl text-center px-4 py-2 m-2">Coming soon...</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
