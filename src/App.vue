95b43a4d1303e83cfeb00b9c7e25b5c15a8b48b6
Mahima Ramgolam <mahima@ramgolam.com>
2020-06-04 00:17:58 +0400

    work in progress responsive

--- src/App.vue
+++ src/App.vue
@@ -15,23 +15,93 @@
 
 <!--working on version-->
   
+<!--grid used -->
+<!-- <div class="bg-yellow-100 min-h-screen" style="background-image: url(images/flower.jpg)" >
+<div class="grid grid-flow-row sm:grid-flow-row md:grid-flow-row-dense lg:grid-col xl:grid-flow-col">
+  <div  class="min-h-screen">
+      <div class="grid grid-flow-row sm:grid-flow-row md:grid-flow-row-dense lg:grid-col xl:grid-flow-col">
+        <div class="relative min-h-screen">
+          <div class="relative bg-white flex lg:justify-end md:justify-center justify-center lg:min-h-screen"> 
+            <div class="relative bg-white lg:min-h-full py-12">
+                  <div class="flex flex-col text-gray-700 text-center">
+                    <div class="fade-in-1  animation-delay text-orange-600 text-center text-xl md:text-3xl lg:text-5xl lg:px-4 lg:pt-8 lg:mx-2 lg:mt-8" style="--delay: 500ms" >Sathya Sai</div>
+                    <div class="fade-in-1 animation-delay text-orange-600 text-center text-xl md:text-3xl lg:text-5xl lg:px-4 lg:mb-2 lg:mb-4" style="--delay: 500ms">International Organisation</div>
+                  </div>
+                    <div class="fade-in-1 animation-delay text-orange-600 text-center font-bold text-2xl md:text-4xl lg:text-6xl lg:px-4 lg:py-2 md:pt-8 lg:mx-2"  style="--delay:1000ms">Mauritius</div>
+                    <div class="fade-in-1  animation-delay text-orange-600 text-center px-4 py-8 m-2 " style="--delay: 1500ms">
+                        <img class="mx-auto h-full w-48 md:w-auto" src="http://saibabamauritius.org/sites/all/themes/surya/logo.png"/>
+                    </div>    
+                    <div class="fade-in-1 animation-delay text-orange-600 font-medium text-xl md:text-4xl lg:text-5xl text-center lg:px-4 lg:py-2 lg:md:py-8 lg:mx-2 mb-2" style="--delay: 2s">Coming soon...</div>             
+            </div>
+          </div>
+        </div>
+        <div class="bg--200 min-h-screen">
+           <div class="relative text-center -ml-2 mr-2 h-0 w-0 md:h-0 md:w-0 lg:h-1200 lg:w-100">
+              <svg class="absolute top-0 left-0 md:overflow-hidden" viewBox="0 0 100 1200" fill="#FFFFFF" xmlns="http://www.w3.org/2000/svg">
+                <polygon points="0,0 100,0 0,1200"/>
+              </svg>
+            </div>
+        </div>
+      </div>  
+  </div>
+  
+</div>
 
-<div class="lg:flex flex-shrink-0 lg:min-h-screen bg-repeat" style="background-image: url(images/flower2.jpg)" >
+</div> -->
 
-  <div class="relative w-12/12 lg:w-6/12 text-gray-700 bg-white text-center">
+<!--flexbox width-->
+<div class="" style="background-image: url(images/flower2.jpg)" >
+  <div class="flex flex-wrap">
+    <div class="w-full lg:w-1/2 py-8 lg:py-0">
+
+      <div class="flex flex-wrap">
+        <div class="relative bg-white w-full lg:w-4/5">
+          <div class="relative  flex lg:justify-end md:justify-center justify-center min-h-screen"> 
+            <div class="relative lg:min-h-full pt-12">
+                  <div class="flex flex-col text-gray-700 text-center">
+                    <div class="fade-in-1  animation-delay text-orange-600 text-center text-xl md:text-4xl lg:text-5xl lg:px-4 lg:pt-8 lg:mx-2 lg:mt-8" style="--delay: 500ms" >Sathya Sai</div>
+                    <div class="fade-in-1 animation-delay text-orange-600 text-center text-xl md:text-4xl lg:text-5xl lg:px-4 lg:mb-2 lg:mb-4" style="--delay: 500ms">International Organisation</div>
+                  </div>
+                    <div class="fade-in-1 animation-delay text-orange-600 text-center font-bold text-3xl md:text-6xl lg:text-6xl lg:px-4 lg:py-2 pt-8 md:pt-8 lg:mx-2"  style="--delay:1000ms">Mauritius</div>
+                    <div class="fade-in-1  animation-delay text-orange-600 text-center px-4 py-8 m-2 " style="--delay: 1500ms">
+                        <img class="mx-auto h-full w-48 md:w-auto" src="http://saibabamauritius.org/sites/all/themes/surya/logo.png"/>
+                    </div>    
+                    <div class="fade-in-1 animation-delay text-orange-600 font-medium text-xl md:text-4xl lg:text-5xl text-center lg:px-4 lg:py-2 lg:md:py-8 lg:mx-2 mb-2" style="--delay: 2s">Coming soon...</div>             
+            </div>
+          </div>
+        </div>
+        <div class="w-0 lg:w-1/5 bg--200 overflow-hidden min-h-screen">
+           <div class="relative text-center -ml-2 mr-2 lg:h-1200 lg:w-100">
+              <svg class="absolute top-0 left-0 md:overflow-hidden" viewBox="0 0 100 1200" fill="#FFFFFF" xmlns="http://www.w3.org/2000/svg">
+                <polygon points="0,0 100,0 0,1200"/>
+              </svg>
+            </div>
+        </div>
+      </div>  
+    
+    </div>
+   
+  </div>
+
+</div>
+
+<!--good one-->
+  <!-- 
+    <div class="lg:flex flex-shrink-0 lg:min-h-screen bg-repeat" style="background-image: url(images/flower2.jpg)" >
+    <div class="relative w-12/12 lg:w-6/12 text-gray-700 bg-white text-center">
     <div class=" bg-white relative flex justify-center lg:min-h-screen lg:-mr-20"> 
       <div class="relative bg--300 lg:min-h-full py-12">
             <div class="flex flex-col text-gray-700 text-center">
-              <div class="fade-in-1 text-orange-600 text-center text-xl md:text-3xl lg:text-5xl lg:px-4 lg:pt-8 lg:mx-2 lg:mt-8">Sathya Sai</div>
-              <div class="fade-in-2 text-orange-600 text-center text-xl md:text-3xl lg:text-5xl lg:px-4 lg:mb-2 lg:mb-4">International Organisation</div>
+              <div class="fade-in-1  animation-delay text-orange-600 text-center text-xl md:text-3xl lg:text-5xl lg:px-4 lg:pt-8 lg:mx-2 lg:mt-8" style="--delay: 500ms" >Sathya Sai</div>
+              <div class="fade-in-1 animation-delay text-orange-600 text-center text-xl md:text-3xl lg:text-5xl lg:px-4 lg:mb-2 lg:mb-4" style="--delay: 500ms">International Organisation</div>
             </div>
-              <div class="fade-in-3 text-orange-600 text-center font-bold text-2xl md:text-4xl lg:text-6xl lg:px-4 lg:py-2 md:pt-8 lg:mx-2">Mauritius</div>
-              <div class="fade-in-4 text-orange-600 text-center px-4 py-8 m-2">
+              <div class="fade-in-1 animation-delay text-orange-600 text-center font-bold text-2xl md:text-4xl lg:text-6xl lg:px-4 lg:py-2 md:pt-8 lg:mx-2"  style="--delay:1000ms">Mauritius</div>
+              <div class="fade-in-1  animation-delay text-orange-600 text-center px-4 py-8 m-2 " style="--delay: 1500ms">
                   <img class="mx-auto h-full w-48 md:w-auto" src="http://saibabamauritius.org/sites/all/themes/surya/logo.png"/>
               </div>
       
-              <div class="fade-in-5 text-orange-600 font-medium text-xl md:text-4xl lg:text-5xl text-center lg:px-4 lg:py-2 lg:md:py-8 lg:mx-2 mb-2">Coming soon...</div>
-
+              <div class="fade-in-1 animation-delay text-orange-600 font-medium text-xl md:text-4xl lg:text-5xl text-center lg:px-4 lg:py-2 lg:md:py-8 lg:mx-2 mb-2" style="--delay: 2s">Coming soon...</div>
+             
       </div>
     </div>
   </div>
@@ -42,9 +112,9 @@
     </svg>
   </div>
 
-  <div class="relative overflow-hidden lg:hidden lg:w-5/12 text-gray-700 text-center px-4 py-2 m-2"></div>
-
+</div> 
 </div>
+-->
 
 </template>
 
@@ -55,19 +125,17 @@ export default {
 </script>
 
 <style lang="scss">
-* {
-  padding: 0;
-  border: 0;
-  margin: 0;
-}
 
 .fade-in-1 {
-  animation: fadeIn ease 2s;
-  -webkit-animation: fadeIn ease 2s;
-  -moz-animation: fadeIn ease 2s;
-  -o-animation: fadeIn ease 2s;
-  -ms-animation: fadeIn ease 2s;
+  opacity:0;
+  animation: fadeIn ease 1s forwards;
 }
+
+.animation-delay {
+  animation-delay: var(--delay, 0.5s); 
+}
+
+
 @keyframes fadeIn {
   0% {
     opacity:0;
@@ -77,244 +145,5 @@ export default {
   }
 }
 
-@-moz-keyframes fadeIn {
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-  }
-}
 
-@-webkit-keyframes fadeIn {
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-  }
-}
-
-@-o-keyframes fadeIn {
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-  }
-}
-
-@-ms-keyframes fadeIn {
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-}
-}
-
-.fade-in-2 {
-  animation: fadeIn ease 4s;
-  -webkit-animation: fadeIn ease 4s;
-  -moz-animation: fadeIn ease 4s;
-  -o-animation: fadeIn ease 4s;
-  -ms-animation: fadeIn ease 4s;
-}
-@keyframes fadeIn {
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-  }
-}
-
-@-moz-keyframes fadeIn {
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-  }
-}
-
-@-webkit-keyframes fadeIn {
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-  }
-}
-
-@-o-keyframes fadeIn {
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-  }
-}
-
-@-ms-keyframes fadeIn {
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-}
-}
-.fade-in-3 {
-  animation: fadeIn ease 6s;
-  -webkit-animation: fadeIn ease 6s;
-  -moz-animation: fadeIn ease 6s;
-  -o-animation: fadeIn ease 6s;
-  -ms-animation: fadeIn ease 6s;
-}
-@keyframes fadeIn {
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-  }
-}
-
-@-moz-keyframes fadeIn {
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-  }
-}
-
-@-webkit-keyframes fadeIn {
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-  }
-}
-
-@-o-keyframes fadeIn {
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-  }
-}
-
-@-ms-keyframes fadeIn {
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-}
-}
-.fade-in-4 {
-  animation: fadeIn ease 8s;
-  -webkit-animation: fadeIn ease 8s;
-  -moz-animation: fadeIn ease 8s;
-  -o-animation: fadeIn ease 8s;
-  -ms-animation: fadeIn ease 8s;
-}
-@keyframes fadeIn {
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-  }
-}
-
-@-moz-keyframes fadeIn {
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-  }
-}
-
-@-webkit-keyframes fadeIn {
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-  }
-}
-
-@-o-keyframes fadeIn {
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-  }
-}
-
-@-ms-keyframes fadeIn {
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-}
-}
-.fade-in-5 {
-  animation: fadeIn ease 10s;
-  -webkit-animation: fadeIn ease 10s;
-  -moz-animation: fadeIn ease 10s;
-  -o-animation: fadeIn ease 10s;
-  -ms-animation: fadeIn ease 10s;
-}
-@keyframes fadeIn {
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-  }
-}
-
-@-moz-keyframes fadeIn {
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-  }
-}
-
-@-webkit-keyframes fadeIn {
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-  }
-}
-
-@-o-keyframes fadeIn {
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-  }
-}
-
-@-ms-keyframes fadeIn {
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-}
-}
 </style>
